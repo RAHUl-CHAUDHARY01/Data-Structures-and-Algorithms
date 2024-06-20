ee032597f1fd850fae3d7640079fb797bb1fed86
Rahul Kumar <132205464+RAHUl-CHAUDHARY01@users.noreply.github.com>
2024-06-20 23:59:37 +0530

    Create 7.Binary Search Tree to Greater Sum Tree.cpp

---
+++ Binary Search Tree/7.Binary Search Tree to Greater Sum Tree.cpp
@@ -0,0 +1,16 @@
+    void rinorder(TreeNode* root, int &sum)
+    {
+        if(root==NULL) return;
+
+        rinorder(root->right, sum);
+        sum+=root->val;
+        root->val = sum;
+        rinorder(root->left, sum);
+    }
+
+    TreeNode* bstToGst(TreeNode* root) {
+        int sum=0;
+        rinorder(root, sum);
+
+        return root;
+    }
